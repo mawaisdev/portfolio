6b16cddfcc461fcd7fdf9ac7f714ba84ef98fe7e
Muhammad Awais <usamamushtaq.dev@gmail.com>
2025-10-26 05:50:25 +0500

    fix: remove deprecated swcMinify option from Next.js config - SWC minification is enabled by default in Next.js 15 - Remove deprecated swcMinify config key

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: true,
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
